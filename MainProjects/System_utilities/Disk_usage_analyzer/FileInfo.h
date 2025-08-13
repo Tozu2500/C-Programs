485664f8d9002b11c4812a970d71dce2d92c9a3e
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-08-13 07:37:44 +0300

    Update FileInfo.h

--- MainProjects/System_utilities/Disk_usage_analyzer/FileInfo.h
+++ MainProjects/System_utilities/Disk_usage_analyzer/FileInfo.h
@@ -13,6 +13,18 @@ public:
     std::string getName() const;
     uint64_t getSize() const;
     bool isDirectory() const;
+    void setSize(uint64_t size);
+    void addChild(const FileInfo& child);
+    std::vector<FileInfo>& getChildren() const;
+    void sortChildren();
+    std::string getFormattedSize() const;
+    double getPercentage(uint64_t totalSize) const;
+
+private:
+    std::string name;
+    uint64_t size;
+    bool directory;
+    std::vector<FileInfo> children;
 };
 
 #endif // FILEINFO_H
