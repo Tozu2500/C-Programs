cf6a200c4dea168559bc0bb4c9d8f26ab1532292
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-09-30 02:30:53 +0300

    Add files via upload

---
+++ Practice/Intermediate/expense_tracker/Transaction.cpp
@@ -0,0 +1,73 @@
+#include "Transaction.h"
+#include "Utils.h"
+#include <sstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+Transaction::Transaction()
+{
+    id = 0;
+    amount = 0.0;
+    recurring = false;
+    recurrence = "none";
+}
+
+string Transaction::serialize() const
+{
+    auto esc = [](const string &s)
+    {
+        string r;
+        for (char c : s)
+        {
+            if (c == '|') r += "\\|";
+            else r += c;
+        }
+        return r;
+    };
+    ostringstream ss;
+    ss << "TID=" << id;
+    ss << "|DATE=" << date;
+    ss << "|AMT=" << fixed << setprecision(2) << amount;
+    ss << "|CAT=" << esc(category);
+    ss << "|NOTE=" << esc(note);
+    ss << "|ACC=" << esc(account);
+    ss << "|REC=" << (recurring ? "1" : "0");
+    ss << "|RINT" << recurrence;
+    return ss.str();
+}
+
+Transaction Transaction::deserialize(const string &line)
+{
+    Transaction t;
+    vector<string> parts;
+    string cur;
+    bool esc = false;
+    for (size_t i = 0; i < line.size(); ++i)
+    {
+        char c = line[i];
+        if (esc)
+        {
+            if (c == '|') cur.push_back('|');
+            else if (c == 'n') cur.push_back('\n');
+            else cur.push_back(c);
+            esc = false;
+        }
+        else
+        {
+            if (c == '\\') esc = true;
+            else if (c == '|')
+            {
+                parts.push_back(cur);
+                cur.clear();
+            }
+            else cur.push_back(c);
+        }
+    }
+    if (!cur.empty()) parts.push_back(cur);
+    for (auto &p : parts)
+    {
+        
+    }
+}
