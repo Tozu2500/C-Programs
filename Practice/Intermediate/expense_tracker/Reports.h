e834a4a1afdd7df54153eaa02c14250c39856901
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-10-01 20:40:14 +0300

    Add files via upload

--- Practice/Intermediate/expense_tracker/Reports.h
+++ Practice/Intermediate/expense_tracker/Reports.h
@@ -3,5 +3,5 @@
 #include <string>
 
 void projection_report(const AppState &S, int years, double expected_annual_return, double annual_contribution);
-void mothly_summary(const AppState &S, const std::string &year_month);
+void monthly_summary(const AppState &S, const std::string &year_month);
 void export_csv(const AppState &S, const std::string &fname = "export.csv");
